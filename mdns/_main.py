a6fe0d62be31dbfe7cb700f0fb5de54c8426bd97
Vincent Audric <vaudric@gmail.com>
2021-05-30 16:35:11 -0400

    Add files via upload

---
+++ mdns/_main.py
@@ -0,0 +1,45 @@
+import uasyncio as asyncio
+import picoweb
+from mdns import SlimDNSServer
+from httpd import WebApp
+
+mdns_server = None
+while True:
+    try:
+        from mdns import SlimDNSServer    
+        mdns_server = SlimDNSServer(wlan.ifconfig()[0], "esp8266-upy")
+        break
+    except OSError:
+        continue
+
+app = WebApp(__name__)
+
+
+@app.route("/")
+def index(req, resp):
+    yield from picoweb.start_response(resp)
+    yield from resp.awrite("This is a webapp")
+
+
+async def check_free_mem():
+    while True:
+        gc.collect()
+        print('Free MEM: {}'.format(gc.mem_free()))
+        gc.collect()
+        await asyncio.sleep(5)
+
+def run():
+    loop = asyncio.get_event_loop()
+    loop.create_task(mdns_server.serve())
+    app.run(debug=True, host="0.0.0.0", port=80, main_loop=False)
+    loop.create_task(check_free_mem())
+    loop.run_forever()
+
+
+try:
+    run()
+except KeyboardInterrupt:
+    print('Program ends!')
+finally:
+    del mdns_server, app
+    _ = asyncio.new_event_loop()
